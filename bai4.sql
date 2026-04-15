42688856d1492cc0f75f910e31697701e48a365e
datphung181 <datphung1801@gmail.com>
2026-04-15 18:57:19 +0700

    Add files via upload

---
+++ bai4.sql
@@ -0,0 +1,19 @@
+-- có 2 cách:
+	-- 1 là xóa bảng bằng drop rôi  viết lại
+    -- 2 là dùng alter để sửa
+
+-- cách 1 sẽ nhanh nhưng sẽ làm mất đi tất cả dữ liệu đã truyền vào
+-- cách 2 sẽ vất vả hơn nhưng k làm mất đi dữ liệu sãn có
+
+
+CREATE DATABASE users_database;
+USE users_database; 
+
+CREATE TABLE users(
+	id_user INT PRIMARY KEY,
+    name_user VARCHAR(100) NOT NULL,
+	phone INT 
+
+);
+ALTER TABLE users
+MODIFY COLUMN phone VARCHAR(50);
